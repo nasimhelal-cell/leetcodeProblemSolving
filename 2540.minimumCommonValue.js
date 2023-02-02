0f91f86e0c28796313915ff5611f883a809b2a0b
Nasim <nasimhelal123>
2023-02-02 11:08:53 +0600

    '2540. Minimum Common Value'   is solved

---
+++ 2540.minimumCommonValue.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var getCommon = function (nums1, nums2) {
+  let count = 0;
+  for (let i = 0; i < nums1.length; i++) {
+    for (let j = 0; j < nums2.length; j++) {
+      if (nums1[i] === nums2[j]) {
+        count++;
+        return nums1[i];
+      }
+    }
+  }
+  if (count === 0) return -1;
+};
