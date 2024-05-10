d610a4b1285e28be9f3870db35d98f36f8ee42d5
Nasim <nasimhelal123>
2024-05-10 17:26:35 +0600

    27.removeElement is solved

---
+++ 27.removeElement.js
@@ -0,0 +1,12 @@
+var removeElement = function (nums, val) {
+  let index = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== val) {
+      nums[index++] = nums[i];
+    }
+  }
+  return index;
+};
+
+console.log(removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2));
